19e0a1f066977880a0834f45328add137814e9ea
ccozad <ccozad@gmail.com>
2024-07-30 17:51:21 -0700

    Bar chart and line chart resources

--- vue/src/views/BarChartExample.vue
+++ vue/src/views/BarChartExample.vue
@@ -4,16 +4,27 @@ import BarChart from '@/components/BarChart.vue';
 
 <template>
   <div>
-    <h1>Simple Bar Chart</h1>
-    <div class="container-fluid p-2">
+    <h1 class="p-3">Simple Bar Chart</h1>
+    <div class="container-fluid">
         <div class="row">
             <div class="col-9">
                 <BarChart />
             </div>
             <div class="col-3">
-                This is an example of a bar chart
+                <h3>
+                    Resources
+                </h3>
+                <ul>
+                    <li>
+                        <a href="https://github.com/ccozad/vue3-apex-charts-reference/blob/main/vue/src/components/BarChart.vue" target="_blank">Source Code</a>
+                    </li>
+                    <li>
+                        <a href="https://apexcharts.com/docs/chart-types/bar-chart/" target="_blank">Bar Chart Documentation</a>
+                    </li>
+                </ul>
             </div>
         </div>
+        
     </div>
   </div>
 </template>
--- vue/src/views/LineChartExample.vue
+++ vue/src/views/LineChartExample.vue
@@ -4,14 +4,24 @@ import LineChart from '@/components/LineChart.vue';
 
 <template>
   <div>
-    <h1>Simple Line Chart</h1>
+    <h1 class="p-3">Simple Line Chart</h1>
     <div class="container-fluid p-2">
         <div class="row">
             <div class="col-9">
                 <LineChart />
             </div>
             <div class="col-3">
-                This is an example of a line chart
+                <h3>
+                    Resources
+                </h3>
+                <ul>
+                    <li>
+                        <a href="https://github.com/ccozad/vue3-apex-charts-reference/blob/main/vue/src/components/LineChart.vue" target="_blank">Source Code</a>
+                    </li>
+                    <li>
+                        <a href="https://apexcharts.com/docs/chart-types/line-chart/" target="_blank">Line Chart Documentation</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
